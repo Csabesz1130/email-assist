dc58e3c9ddcb63344afd0eda212daab92911d172
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-04-29 15:49:18 +0200

    improve

--- src/services/calendarService.mjs
+++ src/services/calendarService.mjs
@@ -2,7 +2,7 @@ import Event from '../models/eventModel.mjs';
 
 export const getEvents = async () => {
   try {
-    const events = await Event.find();
+    const events = await Event.find({});
     return events;
   } catch (error) {
     throw new Error('Failed to fetch events');
@@ -12,9 +12,11 @@ export const getEvents = async () => {
 export const getEventById = async (eventId) => {
   try {
     const event = await Event.findById(eventId);
+
     if (!event) {
       throw new Error('Event not found');
     }
+
     return event;
   } catch (error) {
     throw new Error('Failed to fetch event');
@@ -33,9 +35,11 @@ export const createEvent = async (eventData) => {
 export const updateEvent = async (eventId, updateData) => {
   try {
     const updatedEvent = await Event.findByIdAndUpdate(eventId, updateData, { new: true });
+
     if (!updatedEvent) {
       throw new Error('Event not found');
     }
+
     return updatedEvent;
   } catch (error) {
     throw new Error('Failed to update event');
@@ -45,9 +49,11 @@ export const updateEvent = async (eventId, updateData) => {
 export const deleteEvent = async (eventId) => {
   try {
     const deletedEvent = await Event.findByIdAndDelete(eventId);
+
     if (!deletedEvent) {
       throw new Error('Event not found');
     }
+
     return deletedEvent;
   } catch (error) {
     throw new Error('Failed to delete event');
